0577f9ef55dc5823496c5088acc4033216be7ce7
Yellokat <jenova195@gmail.com>
2025-02-24 04:44:40 +0800

    feat : part 3 - exercise 20

--- part-3/phonebook-backend/models/person.js
+++ part-3/phonebook-backend/models/person.js
@@ -20,6 +20,13 @@ const personSchema = new mongoose.Schema({
     },
     number: {
         type: String,
+        minLength: 8,
+        validate: {
+            validator: function(v) {
+                return /^[0-9]{2,3}-[0-9]+$/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
         required: true
     },
 })
