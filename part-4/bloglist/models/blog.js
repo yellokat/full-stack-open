9ca7e1f23120245ff2412b3490aa2cc7ec250dcc
Yellokat <jenova195@gmail.com>
2025-02-27 05:50:31 +0800

    feat : part 4 - exercise 1

---
+++ part-4/bloglist/models/blog.js
@@ -0,0 +1,24 @@
+// noinspection DuplicatedCode
+
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+mongoose.connect(url)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
