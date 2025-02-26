9110c6952c1c39cd90af8fbf12fcf723a878f234
Yellokat <jenova195@gmail.com>
2025-02-27 06:26:41 +0800

    refact : middleware refactoring done

--- part-4/bloglist/index.js
+++ part-4/bloglist/index.js
@@ -4,29 +4,13 @@
 
 require('dotenv').config()
 const express = require('express')
-const morgan = require('morgan')
 const cors = require('cors')
 const Blog = require("./models/blog")
+const middleware = require("./utils/middleware")
 
 // const app = require("./app")
 // const config = require("./utils/config")
 const logger = require("./utils/logger")
-
-// ======================================================
-// middleware configuration
-// ======================================================
-
-// request logger
-const requestLogger = morgan(function (tokens, req, res) {
-  return [
-    tokens.method(req, res),
-    tokens.url(req, res),
-    tokens.status(req, res),
-    tokens.res(req, res, 'content-length'), '-',
-    tokens['response-time'](req, res), 'ms',
-    JSON.stringify(req.body),
-  ].join(' ')
-})
 
 // ======================================================
 // initialize app
@@ -35,7 +19,7 @@ const requestLogger = morgan(function (tokens, req, res) {
 const app = express()
 
 app.use(express.json())
-app.use(requestLogger)
+app.use(middleware.requestLogger)
 app.use(cors())
 
 // ======================================================
@@ -43,23 +27,26 @@ app.use(cors())
 // ======================================================
 
 // get all blogposts
-app.get('/api/blogs', (request, response) => {
+app.get('/api/blogs', (request, response, next) => {
   Blog
     .find({})
     .then(blogs => {
       response.json(blogs)
-    })
+    }).catch(error=>next(error))
 })
 
 // create a blogpost
-app.post('/api/blogs', (request, response) => {
+app.post('/api/blogs', (request, response, next) => {
   const blog = new Blog(request.body)
   blog
     .save()
     .then(result => {
       response.status(201).json(result)
-    })
+    }).catch(error=>next(error))
 })
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 // ======================================================
 // start server
