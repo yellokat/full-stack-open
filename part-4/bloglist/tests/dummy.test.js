831fff865c20547167aa45597559f34b1a0a99f9
Yellokat <jenova195@gmail.com>
2025-02-27 08:43:07 +0800

    feat : part 4 - exercises 5~7

--- part-4/bloglist/tests/dummy.test.js
+++ part-4/bloglist/tests/dummy.test.js
@@ -6,6 +6,38 @@ const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 const Blog = require('../models/blog')
 
+// mock objects
+const blogMock1 = new Blog({
+  "title": "test1",
+  "author": "tester1",
+  "url": "testurl1",
+  "likes": 1
+})
+const blogMock2 = new Blog({
+  "title": "test2",
+  "author": "tester2",
+  "url": "testurl2",
+  "likes": 2
+})
+const blogMock3 = new Blog({
+  "title": "test3",
+  "author": "tester3",
+  "url": "testurl3",
+  "likes": 3
+})
+const blogMock4 = new Blog({
+  "title": "test4",
+  "author": "tester4",
+  "url": "testurl4",
+  "likes": 4
+})
+const blogMock5 = new Blog({
+  "title": "test5",
+  "author": "tester5",
+  "url": "testurl5",
+  "likes": 5
+})
+
 test('dummy returns one', () => {
   const blogs = []
   const result = listHelper.dummy(blogs)
@@ -13,26 +45,6 @@ test('dummy returns one', () => {
 })
 
 describe("total likes", () => {
-  // mocks
-  const blogMock1 = new Blog({
-    "title": "test1",
-    "author": "tester1",
-    "url": "testurl1",
-    "likes": 1
-  })
-  const blogMock2 = new Blog({
-    "title": "test1",
-    "author": "tester1",
-    "url": "testurl1",
-    "likes": 2
-  })
-  const blogMock3 = new Blog({
-    "title": "test1",
-    "author": "tester1",
-    "url": "testurl1",
-    "likes": 3
-  })
-
   test("of an empty list", () => {
     const blogs = []
     const result = listHelper.totalLikes(blogs)
@@ -49,5 +61,77 @@ describe("total likes", () => {
     const blogs = [blogMock1, blogMock2, blogMock3]
     const result = listHelper.totalLikes(blogs)
     assert.strictEqual(result, 6)
+  })
+})
+
+describe("favorite blog", () => {
+  test("of an empty list", () => {
+    const blogs = []
+    const result = listHelper.favoriteBlog(blogs)
+    assert.strictEqual(result, null)
+  })
+
+  test("of a single blog entry", ()=>{
+    const blogs = [blogMock1]
+    const result = listHelper.favoriteBlog(blogs)
+    assert.deepStrictEqual(result, blogMock1)
+  })
+
+  test("of multiple blog entries", () => {
+    const blogs = [blogMock1, blogMock2, blogMock3]
+    const result = listHelper.favoriteBlog(blogs)
+    assert.deepStrictEqual(result, blogMock3)
+  })
+})
+
+describe("most blogs", ()=>{
+  test("of an empty list", () => {
+    const blogs = []
+    const result = listHelper.mostBlogs(blogs)
+    assert.strictEqual(result, null)
+  })
+
+  test("of a single blog entry", ()=>{
+    const blogs = [blogMock1]
+    const result = listHelper.mostBlogs(blogs)
+    assert.deepStrictEqual(result, {
+      author: blogMock1.author,
+      blogs: 1
+    })
+  })
+
+  test("of multiple blog entries", () => {
+    const blogs = [blogMock1, blogMock1, blogMock2, blogMock2, blogMock2, blogMock3]
+    const result = listHelper.mostBlogs(blogs)
+    assert.deepStrictEqual(result, {
+      author: blogMock2.author,
+      blogs: 3
+    })
+  })
+})
+
+describe("most liked blogs", ()=> {
+  test("of an empty list", () => {
+    const blogs = []
+    const result = listHelper.mostLikes(blogs)
+    assert.strictEqual(result, null)
+  })
+
+  test("of a single blog entry", ()=>{
+    const blogs = [blogMock1]
+    const result = listHelper.mostLikes(blogs)
+    assert.deepStrictEqual(result, {
+      author: blogMock1.author,
+      likes: blogMock1.likes
+    })
+  })
+
+  test("of multiple blog entries", ()=>{
+    const blogs = [blogMock1, blogMock1, blogMock2, blogMock2, blogMock2, blogMock3]
+    const result = listHelper.mostLikes(blogs)
+    assert.deepStrictEqual(result, {
+      author: blogMock2.author,
+      likes: 6
+    })
   })
 })
--- part-4/bloglist/utils/list_helper.js
+++ part-4/bloglist/utils/list_helper.js
@@ -1,3 +1,6 @@
+// noinspection DuplicatedCode
+_ = require('lodash')
+
 const dummy = (blogs) => {
   return 1
 }
@@ -14,7 +17,71 @@ const totalLikes = (blogs) => {
   })
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+
+  // helper function
+  function indexOfMax(arr) {
+    if (arr.length === 0) {
+      return -1;
+    }
+    let max = arr[0];
+    let maxIndex = 0;
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] > max) {
+        maxIndex = i;
+        max = arr[i];
+      }
+    }
+    return maxIndex;
+  }
+
+  const likes = blogs.map((blog) =>
+    blog.likes
+  )
+  const maxIdx = indexOfMax(likes)
+  return blogs[maxIdx]
+}
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+
+  let mostBlogAuthor = _(blogs)
+    .countBy(e => e.author)
+    .toPairs()
+    .maxBy(1)
+
+  return {
+    author: mostBlogAuthor[0],
+    blogs: mostBlogAuthor[1]
+  }
+}
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+
+  let mostLikesAuthor = _(blogs)
+    .groupBy(e => e.author)
+    .toPairs()
+    .map((e) => [e[0], _(e[1]).sumBy(e => e.likes)])
+    .maxBy(1)
+
+  return {
+    author: mostLikesAuthor[0],
+    likes: mostLikesAuthor[1]
+  }
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
