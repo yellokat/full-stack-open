b8b0f3ca4f4e35ae1f145bfd756f7422c5325376
Yellokat <jenova195@gmail.com>
2025-03-02 03:42:29 +0800

    feat : part 4 - exercise 18

--- part-4/bloglist/app.js
+++ part-4/bloglist/app.js
@@ -8,6 +8,7 @@ const cors = require('cors')
 const middleware = require("./utils/middleware")
 const blogsRouter = require("./controllers/blogs")
 const usersRouter = require('./controllers/users')
+const loginRouter = require("./controllers/login")
 const mongoose = require("mongoose");
 
 
@@ -43,6 +44,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
