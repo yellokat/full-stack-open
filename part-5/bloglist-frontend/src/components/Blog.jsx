f3006a14c7741beec381acc5de6fac1a3a09147e
Yellokat <jenova195@gmail.com>
2025-03-03 00:22:21 +0800

    feat : part 5 - exercise 7

--- part-5/bloglist-frontend/src/components/Blog.jsx
+++ part-5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import {useState} from "react";
+
+const Blog = ({blog}) => {
+  const [expanded, setExpanded] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    expanded ?
+      <div style={blogStyle}>
+        {blog.title}
+        <button onClick={() => setExpanded(false)}>hide</button>
+        <br/>
+        {blog.url}
+        <br/>
+        {blog.likes}
+        <button onClick={() => {
+        }}>like
+        </button>
+        <br/>
+        {blog.author}
+      </div> :
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setExpanded(true)}>view</button>
+      </div>
+  )
+}
 
 export default Blog
