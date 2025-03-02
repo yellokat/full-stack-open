d800e37b76700f664cb1b271573fe68a1b906522
Yellokat <jenova195@gmail.com>
2025-03-03 05:15:11 +0800

    feat : part 5 - exercise 15

--- part-5/bloglist-frontend/src/components/Blog.test.jsx
+++ part-5/bloglist-frontend/src/components/Blog.test.jsx
@@ -71,3 +71,36 @@ test('when blog component is expanded, url & likes are also visible.', async () 
   expect(likesElement).toBeDefined()
 })
 
+test('when like button is pressed twice, the event handler is also called twice.', async () => {
+  const testBlogData = {
+    'title': 'test title',
+    'author': 'test author',
+    'url': 'test url',
+    'likes': 98765
+  }
+
+  const testUserData = {
+    'username': 'testusername',
+    'user': 'testuser'
+  }
+
+  const mockOnUpdateHandler = vi.fn()
+  const mockOnDeleteHandler = vi.fn()
+
+  const { container } = render(<Blog blog={testBlogData}
+    currentUser={testUserData}
+    onUpdate={mockOnUpdateHandler}
+    onRemove={mockOnDeleteHandler}/>)
+
+  // click on button to expand content
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  // click like button twice
+  const likeButton = screen.getByText('like')
+  await user.click(likeButton)
+  await user.click(likeButton)
+
+  expect(mockOnUpdateHandler.mock.calls).toHaveLength(2)
+})
