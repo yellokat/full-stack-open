4362f26f9d7d9b1f9e6fbac3ad585423b310802a
Yellokat <jenova195@gmail.com>
2025-03-08 02:31:38 +0200

    feat : part 6 - exercise 22

--- part-6/anecdotes-react-query/src/App.jsx
+++ part-6/anecdotes-react-query/src/App.jsx
@@ -1,9 +1,18 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
-import { useQuery } from '@tanstack/react-query'
-import { getAnecdotes } from './requests.jsx'
+import {useMutation, useQuery, useQueryClient} from '@tanstack/react-query'
+import {getAnecdotes, voteAnecdote} from './requests.jsx'
 
 const App = () => {
+  const queryClient = useQueryClient()
+
+  const updateMutation = useMutation({
+    mutationFn: voteAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] });
+    },
+  })
+
   const result = useQuery({
     queryKey: ['anecdotes'],
     queryFn: getAnecdotes,
@@ -18,8 +27,13 @@ const App = () => {
     return <div>anecdote service not available due to server error</div>
   }
 
-  const handleVote = (id)=>{
 
+
+  const handleVote = (anecdote)=>{
+    updateMutation.mutate({
+      ...anecdote,
+      votes: anecdote.votes+1
+    })
   }
 
   const anecdotes = result.data
--- part-6/anecdotes-react-query/src/requests.jsx
+++ part-6/anecdotes-react-query/src/requests.jsx
@@ -5,11 +5,21 @@ const baseUrl = 'http://localhost:3001/anecdotes'
 export const getAnecdotes = () => {
   return axios
     .get(baseUrl)
-    .then(res => res.data)
+    .then(res => {
+      const anecdotes = res.data
+      anecdotes.sort((a, b) => b.votes - a.votes)
+      return anecdotes;
+    })
 }
 
 export const createAnecdote = (newAnecdote) => {
   return axios
     .post(baseUrl, newAnecdote)
+    .then(res => res.data)
+}
+
+export const voteAnecdote = (updatedAnecdote) => {
+  return axios
+    .put(`${baseUrl}/${updatedAnecdote.id}`, updatedAnecdote)
     .then(res => res.data)
 }
