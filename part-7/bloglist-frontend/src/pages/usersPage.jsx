c6790774aa4e7435742f1e9243849220c7c2a6db
Yellokat <jenova195@gmail.com>
2025-03-09 18:37:43 +0200

    feat : part 7 - exercise 14

--- part-7/bloglist-frontend/src/pages/usersPage.jsx
+++ part-7/bloglist-frontend/src/pages/usersPage.jsx
@@ -1,12 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { logout } from '../reducers/userSlice.js'
+import userService from '../services/users'
 
 function UsersPage() {
   const user = useSelector((state) => state.user)
   const navigate = useNavigate()
   const dispatch = useDispatch()
+
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    if (user.token) {
+      userService.getAll(user.token).then((response) => setUsers(response))
+    }
+  }, [user])
 
   // useEffect(() => {
   //   if (!user.name) {
@@ -31,10 +39,10 @@ function UsersPage() {
           </tr>
         </thead>
         <tbody>
-          {data.map((item, index) => (
-            <tr key={index}>
-              <td>hello</td>
-              <td>world</td>
+          {data.map((user) => (
+            <tr key={user.username}>
+              <td>{user.name}</td>
+              <td>{user.blogs.length}</td>
             </tr>
           ))}
         </tbody>
@@ -48,7 +56,7 @@ function UsersPage() {
       <span>{user.name} logged in</span>
       <button onClick={handleLogout}>logout</button>
       <h2>Users</h2>
-      <CustomTable data={[1, 1, 1]} />
+      <CustomTable data={users} />
     </div>
   )
 }
