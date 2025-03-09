dd3dfae3af62bff90020ef3d93493ba9414d8f76
Yellokat <jenova195@gmail.com>
2025-03-09 18:19:09 +0200

    feat : implemented simple navigation menu

--- part-7/bloglist-frontend/src/App.jsx
+++ part-7/bloglist-frontend/src/App.jsx
@@ -1,17 +1,48 @@
 import BlogsPage from './pages/blogsPage.jsx'
 import LoginPage from './pages/loginPage.jsx'
-import { BrowserRouter as Router,Route, Routes } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Routes,
+  useLocation,
+  useNavigate,
+} from 'react-router-dom'
 import UsersPage from './pages/usersPage.jsx'
+
+const ConditionalNavigationMenu = ({ children }) => {
+  const location = useLocation()
+  const navigate = useNavigate()
+
+  const NavigationMenu = () => {
+    return (
+      <>
+        <Link to={'/blogs'}>blogs</Link>
+        <Link to={'/users'}>users</Link>
+      </>
+    )
+  }
+  return (
+    <>
+      {location.pathname !== '/' ? <NavigationMenu/> : null}
+      {children}
+    </>
+  )
+}
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage/>}/>
-        <Route path="/blogs" element={<BlogsPage />} />
-        <Route path="/users" element={<UsersPage />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <ConditionalNavigationMenu>
+          <Routes>
+            <Route path="/" element={<LoginPage />} />;
+            <Route path="/blogs" element={<BlogsPage />} />
+            <Route path="/users" element={<UsersPage />} />
+          </Routes>
+        </ConditionalNavigationMenu>
+      </Router>
+    </>
   )
 }
 
--- part-7/bloglist-frontend/src/pages/usersPage.jsx
+++ part-7/bloglist-frontend/src/pages/usersPage.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
-function UsersPage(props) {
-  return (
-    <div></div>
-  )
+function UsersPage() {
+  const user = useSelector((state) => state.user)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!user.name) {
+      window.alert('You are not logged in.')
+      navigate('/')
+    }
+  }, [user])
+
+  return <div></div>
 }
 
 export default UsersPage
