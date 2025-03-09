fe273010890e4db3a8b5c2dc1c379f3a8fe7447b
Yellokat <jenova195@gmail.com>
2025-03-09 18:27:46 +0200

    feat : implemented table placeholder

--- part-7/bloglist-frontend/src/pages/usersPage.jsx
+++ part-7/bloglist-frontend/src/pages/usersPage.jsx
@@ -1,19 +1,56 @@
 import React, { useEffect } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
+import { logout } from '../reducers/userSlice.js'
 
 function UsersPage() {
   const user = useSelector((state) => state.user)
   const navigate = useNavigate()
+  const dispatch = useDispatch()
 
-  useEffect(() => {
-    if (!user.name) {
-      window.alert('You are not logged in.')
-      navigate('/')
-    }
-  }, [user])
+  // useEffect(() => {
+  //   if (!user.name) {
+  //     window.alert('You are not logged in.')
+  //     navigate('/')
+  //   }
+  // }, [user])
 
-  return <div></div>
+  const handleLogout = async (event) => {
+    event.preventDefault()
+    await dispatch(logout())
+    window.localStorage.removeItem('activeUser')
+  }
+
+  const CustomTable = ({ data }) => {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Blogs created</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td>hello</td>
+              <td>world</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    )
+  }
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      <span>{user.name} logged in</span>
+      <button onClick={handleLogout}>logout</button>
+      <h2>Users</h2>
+      <CustomTable data={[1, 1, 1]} />
+    </div>
+  )
 }
 
 export default UsersPage
