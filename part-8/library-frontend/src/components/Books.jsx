5affa3609e3627cf3e8e026057d2f869e33750e4
Yellokat <jenova195@gmail.com>
2025-03-10 12:57:39 +0200

    feat : boilerplate code for exercises 8~12

---
+++ part-8/library-frontend/src/components/Books.jsx
@@ -0,0 +1,38 @@
+import PropTypes from "prop-types";
+
+const Books = (props) => {
+  if (!props.show) {
+    return null
+  }
+
+  const books = []
+
+  return (
+    <div>
+      <h2>books</h2>
+
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>author</th>
+            <th>published</th>
+          </tr>
+          {books.map((a) => (
+            <tr key={a.title}>
+              <td>{a.title}</td>
+              <td>{a.author}</td>
+              <td>{a.published}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+Books.propTypes = {
+  show: PropTypes.bool.isRequired,
+}
+
+export default Books
---
+++ part-8/library-frontend/src/components/NewBook.jsx
@@ -0,0 +1,77 @@
+import { useState } from 'react'
+import PropTypes from "prop-types";
+
+const NewBook = (props) => {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [published, setPublished] = useState('')
+  const [genre, setGenre] = useState('')
+  const [genres, setGenres] = useState([])
+
+  if (!props.show) {
+    return null
+  }
+
+  const submit = async (event) => {
+    event.preventDefault()
+
+    console.log('add book...')
+
+    setTitle('')
+    setPublished('')
+    setAuthor('')
+    setGenres([])
+    setGenre('')
+  }
+
+  const addGenre = () => {
+    setGenres(genres.concat(genre))
+    setGenre('')
+  }
+
+  return (
+    <div>
+      <form onSubmit={submit}>
+        <div>
+          title
+          <input
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+        <div>
+          author
+          <input
+            value={author}
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+        </div>
+        <div>
+          published
+          <input
+            type="number"
+            value={published}
+            onChange={({ target }) => setPublished(target.value)}
+          />
+        </div>
+        <div>
+          <input
+            value={genre}
+            onChange={({ target }) => setGenre(target.value)}
+          />
+          <button onClick={addGenre} type="button">
+            add genre
+          </button>
+        </div>
+        <div>genres: {genres.join(' ')}</div>
+        <button type="submit">create book</button>
+      </form>
+    </div>
+  )
+}
+
+NewBook.propTypes = {
+  show: PropTypes.bool.isRequired,
+}
+
+export default NewBook
